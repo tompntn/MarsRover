0884adbe723d3759b59781a5e7f56c61c5c0dc76
Thomas Pointon <thomas.pointon16@imperial.ac.uk>
2018-01-26 23:52:08 +0000

    Test for a wrapping world.

--- test/RoverTest.java
+++ test/RoverTest.java
@@ -27,30 +27,31 @@ public class RoverTest {
 
  }
 
- @Test
-  public void InvalidPositionTest() {
-
-   // Going backwards off the map
-   try {
-     rover.command('B');
-     fail("Expected a RuntimeException to be thrown");
-   } catch (RuntimeException e) {
-     assertThat(e.getMessage(),
-         containsString("Invalid position"));
-   }
-
-   // Going forward off the map
-   try {
-     for (int i = 0; i < 101; ++i) {
-       rover.command('F');
-     }
-     fail("Expected a RuntimeException to be thrown");
-   } catch (RuntimeException e) {
-     assertThat(e.getMessage(),
-         containsString("Invalid position"));
-   }
-
- }
+ /* This test is no longer needed, as wrapping is implemented. */
+// @Test
+//  public void InvalidPositionTest() {
+//
+//   // Going backwards off the map
+//   try {
+//     rover.command('B');
+//     fail("Expected a RuntimeException to be thrown");
+//   } catch (RuntimeException e) {
+//     assertThat(e.getMessage(),
+//         containsString("Invalid position"));
+//   }
+//
+//   // Going forward off the map
+//   try {
+//     for (int i = 0; i < 101; ++i) {
+//       rover.command('F');
+//     }
+//     fail("Expected a RuntimeException to be thrown");
+//   } catch (RuntimeException e) {
+//     assertThat(e.getMessage(),
+//         containsString("Invalid position"));
+//   }
+//
+// }
 
  @Test
   public void BasicTurnLeftTest() {
@@ -90,6 +91,25 @@ public class RoverTest {
    rover.commandString("FFFRFF");
    assertTrue(rover.getPosition().equals(new Position(2, 3, Direction.E)));
 
+ }
+
+ @Test
+  public void WrappingWorldTest() {
+
+   for (int i = 0; i < 120; ++i) {
+
+     rover.command('F');
+
+   }
+   assertTrue(rover.getPosition().equals(new Position(0, 20, Direction.N)));
+
+   rover.command('R');
+   for (int i = 0; i < 120; ++i) {
+
+     rover.command('F');
+
+   }
+   assertTrue(rover.getPosition().equals(new Position(20, 20, Direction.E)));
 
  }
 
